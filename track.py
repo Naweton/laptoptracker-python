a7e6b831f5ebe5539fa966918a426a6388032e0e
Naweton <161488972+Naweton@users.noreply.github.com>
2024-04-09 11:19:56 -0300

    Update track.py

--- track.py
+++ track.py
@@ -1,20 +1,23 @@
 import os
 import pandas as pd
 
+# Get the current working directory
 current_directory = os.getcwd()
 
 # Construct the file path relative to the current working directory
-
 file_name = 'LaptopsTracker.xlsx'
 file_path = os.path.join(current_directory, file_name)
-file_path = r'C:\Python\Excel Laptops\LaptopsTracker.xlsx'
 
+# Read data from the Excel file
 df = pd.read_excel(file_path)
 
+# Filter rows where the value in the 10th column is 'pending'
 pending_rows = df[df.iloc[:, 9].str.lower() == 'pending']
 
+# Extract values from the 3rd column of the filtered rows
 pending_values = pending_rows.iloc[:, 2]
 
+# Print the extracted values
 print("Users who didn't return their devices:")
 print(pending_values)
 
